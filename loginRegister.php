c46027e9ce4fe43f1739e2c583d3f950090168f9
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-05-15 15:38:49 +0100

    Update loginRegister.php

--- loginRegister.php
+++ loginRegister.php
@@ -94,130 +94,102 @@ include("header.php"); //include the header's code
 ?>
 
 <!--JUMBOTRON-->
-<div class="jumbotron" id="jumbotronDiv">
-  <div class="container-fluid">
-    <div class="row align-items-center">
-      <div class="col-lg-7">
-        <h1 id="jumbotronTeddybear"class="text-center jumbotronTeddybear" style=" font-family:Amatic SC, cursive; color:SlateBlue; text-shadow: 1.5px 1px FireBrick; font-weight=bold">Teddy Bear</h1>
-        <br>
-        <h4 class="text-center" id="jumbotronLove" style="font-family:Amatic SC, cursive; color:SlateBlue;"><i>A friend for life</i></h4>
-        <br>
-      </div>
-    </div>
-    <!--div row--> 
-  </div>
-  <!--div container--> 
-</div>
-<!--div jumbotron--> 
-<!--end of jumbotron--> 
-
-<!--LOG IN OR REGISTRATION-->
-<div class="container-fluid">
-  <div class="row">
-    <div class= "col-md-4">
-      <div class="span12">
-        <div class="" id="loginModal">
-          <div class="modal-body">
-            <div class="well"> 
-              <!--nav login and create account options-->
-              <ul class="nav nav-tabs" id="myTab" role="tablist">
-                <li class="nav-item"><a class="nav-link active" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="true" style="font-family:'Open Sans Condensed', sans-serif; color:FireBrick; font-size:22px;">Login</a></li>
-                &nbsp &nbsp
-                <li class="nav-item"><a class="nav-link" id="register-tab" data-toggle="tab" href="#create" role="tab" aria-controls="register" aria-selected="false" style="font-family:'Open Sans Condensed', sans-serif; color:FireBrick; font-size:22px;">Create Account</a></li>
-                <br>
-              </ul>
-              <div id="myTabContent" class="tab-content">
-                <div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab"> <br>
-                  <!--LOGIN FORM-->
-                  <form action="loginRegister.php" method="POST" class="form-horizontal" id="login">
-                    <fieldset>
-                      <div id="legend">
-                        <legend class="" style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-weight:bold;">Login</legend>
-                      </div>
-                      <br>
-                      <div class="control-group"> 
-                        <!-- Username -->
-                        <label class="control-label"  for="username" style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-size:18px;">Username</label>
-                        <div class="controls">
-                          <input name="username" type="text" required class="input-xlarge" id="username" placeholder="username">
-                          <input name="action" value="login" type="hidden">
-                        </div>
-                      </div>
-                      <br>
-                      <div class="control-group"> 
-                        <!-- Password-->
-                        <label class="control-label" for="password" style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-size:18px;">Password</label>
-                        <br>
-                        <div class="controls">
-                          <input name="password" type="password" required class="input-xlarge" id="password" placeholder="password">
-                        </div>
-                      </div>
-                      <br>
-                      <div class="control-group"> 
-                        <!-- Login Button -->
-                        <div class="controls">
-                          <button class="btn btn-success" style="font-family:'Open Sans Condensed', sans-serif; ">Login</button>
-                        </div>
-                      </div>
-                    </fieldset>
-                  </form>
-                  <!--end login form--> 
-                </div>
-                <br>
-                <div class="tab-pane fade" id="create" role="tabpanel" aria-labelledby="register-tab"> 
-                  <!-- CREATE ACCOUNT FORM-->
-                  <form id="tab" method="post" action="loginRegister.php">
-                    <div id="legend">
-                      <legend class="" style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-weight:bold;">Create Account</legend>
-                    </div>
-                    <br>
-                    <!-- Username-->
-                    <label for='username' style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-size:18px;">Username</label>
-                    &nbsp &nbsp
-                    <input name="username" type="text" required class="form-control" placeholder="username" />
-                    <input name="action" value="register" type="hidden">
-                    <br>
-                    <!---USERNAME VALIDATION FUNCTION JS CALLED VALIDATEFORM--> 
-                    <!-- email-->
-                    <label for='email' style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-size:18px;">Email</label>
-                    &nbsp &nbsp &nbsp &nbsp &nbsp
-                    <input type="email" id="email" name="email" class="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder="email" required />
-                    <br>
-                    <!--INFO REQUIRED AND PATTERN OF EMAIL ADDRESS--> 
-                    <!-- Password-->
-                    <label for='password 'style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-size:18px;">Password</label>
-                    &nbsp &nbsp
-                    <input type="password" id="password-register" name="password" class="form-control" pattern=".{6,}" title="Six or more characters" placeholder="password" required />
-                    <br>
-                    <!--INFO REQUIRED AND PATTERN OF PASSWORD--> 
-                    <!-- Address--> 
-                    <!-- Create account button-->
-                    <input type="submit" class="btn btn-primary col-md-4 col-md-offset-10" style="font-family:'Open Sans Condensed', sans-serif;" value="Submit" />
-                  </form>
-                  <!--ed of create account form--> 
-                </div>
-                <!--div tab-pane--> 
-              </div>
-              <!--div myTabContent--> 
+<div class="jumbotron jumbotron-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <h1 class="h1" style="font-family:'Amatic SC', cursive; color:SlateBlue; font-weight:bold;">
+                    Teddy Bear <br><small><i>A friend for life</small></i></h1>
             </div>
-            <!--div well--> 
-          </div>
-          <!--div modal-body--> 
-        </div>
-        <!--div loginModal--> 
-      </div>
-      <!--div span12--> 
-    </div>
-    <!--div col-->
-    <div class="col-md-8"> <br>
-      <br>
-      <br>
-      &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <img src="images\teddy-2439215__340.jpg" class="img-fluid rounded"> </div>
-    <!--div col--> 
-  </div>
-  <!--div row--> 
+        </div><!--div row-->
+    </div><!--div container-->
 </div>
-<!--div container--> 
+<!--end of jumbotron-->
+  
+ <!--LOG IN OR REGISTRATION-->  
+<div class="container-fluid">
+	<div class="row">
+	<div class= "col-md-4">
+			<div class="span12">
+				<div class="" id="loginModal">
+				<div class="modal-body">
+					<div class="well">
+				<!--nav login and create account options-->
+					<ul class="nav nav-tabs" id="myTab" role="tablist">
+						<li class="nav-item"><a class="nav-link active" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="true" style="font-family:'Open Sans Condensed', sans-serif; color:FireBrick; font-size:22px;">Login</a></li> &nbsp &nbsp 
+						<li class="nav-item"><a class="nav-link" id="register-tab" data-toggle="tab" href="#create" role="tab" aria-controls="register" aria-selected="false" style="font-family:'Open Sans Condensed', sans-serif; color:FireBrick; font-size:22px;">Create Account</a></li> <br>
+                  </ul>
+                  <div id="myTabContent" class="tab-content">
+                    <div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab"> 
+					<br>
+					<!--LOGIN FORM-->
+                      <form class="form-horizontal" action="" method="POST">
+                        <fieldset>
+                          <div id="legend">
+                            <legend class="" style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-weight:bold;">Login</legend>
+                          </div> <br>  
+                          <div class="control-group">
+                            <!-- Username -->
+                            <label class="control-label"  for="username" style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-size:18px;">Username</label>
+                            <div class="controls">
+                              <input type="text" id="username" name="username" placeholder="username" class="input-xlarge" style="font-family:'Open Sans Condensed', sans-serif;">
+                            </div>
+                          </div>
+							<br>
+                          <div class="control-group">
+                            <!-- Password-->
+                            <label class="control-label" for="password" style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-size:18px;">Password</label>
+							<br>
+                            <div class="controls">
+                              <input type="password" id="password" name="password" placeholder="password" class="input-xlarge" style="font-family:'Open Sans Condensed', sans-serif;">
+                            </div>
+                          </div>
+     <br>
+     
+                          <div class="control-group">
+                            <!-- Login Button -->
+                            <div class="controls">
+                              <button class="btn btn-success" style="font-family:'Open Sans Condensed', sans-serif; ">Login</button>
+                            </div>
+                          </div>
+                        </fieldset>
+                      </form><!--end login form-->                
+                    </div>
+					<br>
+                    <div class="tab-pane fade" id="create" role="tabpanel" aria-labelledby="register-tab"> 
+					 <!-- CREATE ACCOUNT FORM-->
+					   <form id="tab" method="post" action="">
+					   <div id="legend">
+                            <legend class="" style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-weight:bold;">Create Account</legend>
+                          </div> <br> 
+					   <!-- Username-->
+                        <label for='username' style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-size:18px;">Username</label> &nbsp &nbsp
+                        <input type="text" name="username" class="form-control" placeholder="username" style="font-family:'Open Sans Condensed', sans-serif;"/><br><!---USERNAME VALIDATION FUNCTION JS CALLED VALIDATEFORM-->
+						<!-- email-->
+                        <label for='email' style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-size:18px;">Email</label> &nbsp &nbsp &nbsp &nbsp &nbsp
+                        <input type="email" id="email" name="email" class="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" placeholder="email" required style="font-family:'Open Sans Condensed', sans-serif;"/><br><!--INFO REQUIRED AND PATTERN OF EMAIL ADDRESS-->
+						 <!-- Password-->
+						<label for='password 'style="font-family:'Open Sans Condensed', sans-serif; color:SlateBlue; font-size:18px;">Password</label> &nbsp &nbsp
+                        <input type="password" id="password" name="password" class="form-control" pattern=".{6,}" title="Six or more characters" placeholder="password" required style="font-family:'Open Sans Condensed', sans-serif;"/><br><!--INFO REQUIRED AND PATTERN OF PASSWORD-->
+						 <!-- Address-->
+						 <!-- Create account button-->
+						 <input type="submit" class="btn btn-primary col-md-4 col-md-offset-10" style="font-family:'Open Sans Condensed', sans-serif;" value="Submit" />
+                          
+                        
+                        </form><!--ed of create account form-->
+                    </div><!--div tab-pane-->
+                </div><!--div myTabContent-->
+              </div><!--div well-->
+            </div><!--div modal-body-->
+        </div><!--div loginModal-->
+	</div><!--div span12-->
+	</div> <!--div col-->
+	<div class="col-md-8">
+	<br><br><br>
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <img src="images\teddy-2439215__340.jpg" class="img-fluid rounded"> 
+	</div><!--div col-->
+</div><!--div row-->
+</div><!--div container-->
 <?php
 include("footer.php"); // include the footer's code
 ?>

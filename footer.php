7eccb8491680ba6b075ab3499d3289a310521dcc
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-05-18 12:15:15 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -34,17 +34,30 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script> 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script> 
+
 <script>
 
 <!--JS VALIDATE USERNAME-->
 function validateForm() {
     var usn = document.forms["tab"]["username"].value;
+	var x = document.forms[“tab”][“email”].value;
+	var atpos = x.indexOf(“@”);
+    var dotpos = x.lastIndexOf(“.”);
+
 	
 	if (usn == "") {
 		alert("Username must be filled out");
 		return false;
 	}
+	
+	if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+
+		alert(“Please enter a valid email address.”);
+
+		return false;
+	}
 }
+
 </script>
 
 <script>
